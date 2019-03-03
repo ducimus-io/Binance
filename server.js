0995c8ee16f82ab99717350db8e363e0cc444975
pankaryp <pan.karyp@gmail.com>
2019-03-03 20:36:12 +0200

    Optimize data write functionality

--- server.js
+++ server.js
@@ -8,7 +8,6 @@
 // Require dependencies
 const binance = require('node-binance-api')();
 const fs = require('fs');
-const util = require('util');
 const chalk = require('chalk');
 const figlet = require('figlet');
 
@@ -31,7 +30,6 @@ figlet('Ducimus', function(err, data) {
     log(chalk.cyan(data))
 });
 
-
 /**
  * Establish a websocket connection for a symbol
  * @param currency: Market symbol 
@@ -46,56 +44,62 @@ function wsConnection(currency) {
      */ 
     binance.websockets.depth([currency], (depth) => {
         let {e:eventType, E:eventTime, s:symbol, u:updateId, b:bidDepth, a:askDepth} = depth;
+
         // update of market depth
         log(chalk.blue(symbol + " market depth update"));
 
-        // Keep track of event time
-        log(chalk.magenta("Update id: ") + updateId, chalk.magenta("\nEvent time: ") + eventTime);
+        // Keep track of event time, id and number of asks/bids
+        log(chalk.magenta("Update id: ") + updateId, chalk.magenta("\nEvent time: ") + eventTime, chalk.magenta("\nAsk num: ") 
+        + chalk.yellow(askDepth.length), chalk.magenta("\nBid num: ") +  chalk.yellow(bidDepth.length));
 
-        // Save event time & update id to txt
-        fs.appendFile(`data/${lowCurrency}/${lowCurrency}-md-t.txt`, "\ntime:"+eventTime+"\nid:"+updateId, 'utf-8', 
-            function(err) {
-                if(err) {
-                    return  log(chalk.red(err));
-                }
-        
-                log(chalk.green(`${currency} time & id saved!`));
-            });
+        // Write event time, id and number of asks/bids in txt
+        const timeFile = fs.createWriteStream(`data/${lowCurrency}/${lowCurrency}-md-t.txt`, {flags:'a'});
+        timeFile.on('error', function(err) {
+            log(chalk.red("There is a problem with the file writting."));
+        });
+        timeFile.write("\ntime:"+eventTime+"\nid:"+updateId+"\nask-num:"+askDepth.length+"\nbid-num:"+bidDepth.length); 
+        timeFile.end();
 
-        // Check if ask aarray is empty
+        // Write asks in txt
+        const askFile = fs.createWriteStream(`data/${lowCurrency}/${lowCurrency}-md-a.txt`, {flags:'a'});
+        askFile.on('error', function(err) {
+            log(chalk.red("There is a problem with the file writting."));
+        });
+        // Check if ask array is empty
         if (askDepth.length === 0) {
+            
+            askFile.write('EMPTY\n'); 
+            
             log(chalk.magenta("ask: ") + chalk.red("EMPTY"));
         } else {
-            log(chalk.magenta("ask: ") + askDepth);
-        }
-
-        // Write ask on txt
-        fs.appendFile(`data/${lowCurrency}/${lowCurrency}-md-a.txt`, util.inspect(askDepth, { compact: false}), 'utf-8', 
-            function(err) {
-                if(err) {
-                    return  log(chalk.red(err));
-                }
-        
-                log(chalk.green(`${currency} ask saved!`));
+            askDepth.forEach(function(a) {
+                // format the file
+                askFile.write(a.join(', ') + '\n'); 
+                log(chalk.magenta("ask: ") + a);
             });
+        }
+        askFile.end();
 
+        // Write bids in txt        
+        const bidFile = fs.createWriteStream(`data/${lowCurrency}/${lowCurrency}-md-b.txt`, {flags:'a'});
+        bidFile.on('error', function(err) {
+            log(chalk.red("There is a problem with the file writting."));
+        });
         // Check if bid aarray is empty
         if (bidDepth.length === 0) {
             log(chalk.magenta("bid: ") + chalk.red("EMPTY"));
         } else {
-            log(chalk.magenta("bid: ") + bidDepth);
-        }
-
-        // Write bid on txt
-        fs.appendFile(`data/${lowCurrency}/${lowCurrency}-md-b.txt`, util.inspect(bidDepth, { compact: false}), 'utf-8', 
-            function(err) {
-                if(err) {
-                    return  log(chalk.red(err));
-                }
-            
-                log(chalk.green(`${currency} bid saved!`));
+            bidDepth.forEach(function(b) {
+                // format the file
+                bidFile.write(b.join(', ') + '\n'); 
+                log(chalk.magenta("bid: ") + b);
             });
+            
+        }
+        bidFile.end();
+        
     });
+    
 }
 
 // Start connections
